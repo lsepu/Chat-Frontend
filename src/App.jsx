4798abf42e45dda4c0354caf6cb8a4a5ccade487
lsepu <55538804+lsepu@users.noreply.github.com>
2022-06-25 16:13:35 -0500

    ADD: Check email verification

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect } from "react";
 import "./App.css";
+import { auth } from "./firebase";
 import ChatRoom from "./pages/ChatRoom";
 import Contacts from "./pages/Contacts";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        if(userAuth.emailVerified){
+          console.log("is verified")
+        }
+        else{
+          alert("need verification");
+          signOut(auth);
+        }
+      } else {
+        console.log("logout")
+      }
+    });
+  }, []);
+
   return (
     <div>
       <div className="container">
